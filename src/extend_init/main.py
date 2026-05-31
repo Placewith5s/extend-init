6b978682ed39b27fc61e7f5488b9a96d02f59101
Placewith5s <qsenkaisu@pm.me>
2026-05-31 18:50:46 +0200

    style: do not support windows yet
    Subprocess module does not work well with Windows, even on Git Bash

--- src/extend_init/main.py
+++ src/extend_init/main.py
@@ -1,3 +1,4 @@
+import sys
 import atexit
 
 from extend_init.modules.frontend_template import *
@@ -5,7 +6,14 @@ from extend_init.modules.empty_folders import ask_common_vanilla_frontend
 from extend_init.modules.empty_files import ask_gh_pages
 
 
-def main() -> None:
+def main():
+    print("Platform code:", sys.platform)
+    print("\n")
+
+    if sys.platform == "win32":
+        print("Windows is not supported!")
+        sys.exit(0)
+
     ask_frontend_template()
     ask_common_vanilla_frontend()
     ask_gh_pages()
